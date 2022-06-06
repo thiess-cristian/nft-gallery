fe7f60c426f76683ec0e44fbe10e792c2df8a948
Viki Val <viktorko99@gmail.com>
2022-06-06 17:28:23 +0200

    :zap: SelectableImage

---
+++ components/shared/view/SelectableImage.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-image
+      ratio="1by1"
+      class="image selectable-image"
+      :src="src"
+      alt="Some Image"
+      @click="handleClick"
+      ></b-image>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class SelectableImage extends Vue {
+  @Prop(String) public src!: string
+
+  @Emit('click')
+  handleClick() {
+    console.log('clicked', this.src)
+    return this.src
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+.selectable-image {
+  border: 2px $primary solid;
+  cursor: pointer;
+}
+</style>
