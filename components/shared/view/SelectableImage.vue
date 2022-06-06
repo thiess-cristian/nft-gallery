cae8bfed7f120b8babbb3cbd096a76ac02900235
Viki Val <viktorko99@gmail.com>
2022-06-06 18:42:47 +0200

    :zap: selectable image reacts to select

--- components/shared/view/SelectableImage.vue
+++ components/shared/view/SelectableImage.vue
@@ -2,6 +2,7 @@
   <b-image
       ratio="1by1"
       class="image selectable-image"
+      :class="{ 'is-selected': selected }"
       :src="src"
       alt="Some Image"
       @click.native="handleClick"
@@ -9,24 +10,28 @@
 </template>
 
 <script lang="ts">
-import { Component, Emit, Prop, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
 
 @Component({})
 export default class SelectableImage extends Vue {
   @Prop(String) public src!: string
+  @Prop(Number) public index!: number
+  protected selected = false
 
-  @Emit('click')
   handleClick() {
-    console.log('clicked', this.src)
-    return this.src
+    this.selected = !this.selected
+    this.$emit(this.selected ? 'click' : 'remove', this.index)
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/styles/variables';
-.selectable-image:hover {
+.is-selected {
   border: 2px $primary solid;
+}
+.selectable-image:hover {
+  border: 2px $lightpink solid;
   cursor: pointer;
 }
 </style>
